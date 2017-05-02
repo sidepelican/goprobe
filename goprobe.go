6f241c723a6b81bd6c2d07a857dc0ec1a196614a
kenta_okamura <side.junktown@gmail.com>
2017-05-02 16:07:53 +0900

    add proberecord source

--- goprobe.go
+++ goprobe.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-    "fmt"
     "log"
     //"net/http"
     //"bytes"
     //"github.com/BurntSushi/toml"
     "flag"
 
-    "github.com/google/gopacket"
-    "github.com/google/gopacket/pcap"
-    "github.com/google/gopacket/layers"
     "github.com/sidepelican/goprobe/probe"
 )
 
@@ -21,53 +17,6 @@ type Config struct {
 
 var config Config
 
-func openAvailableMonitorModeInterface() (*pcap.Handle, error) {
-
-    // find useable devices
-    ifs, err := pcap.FindAllDevs()
-    if len(ifs) == 0 {
-        return nil, fmt.Errorf("no devices found : %s\n", err)
-    }
-
-    // try any interfaces to monitor
-    for _, iface := range ifs {
-        handle, err := openAsMonitorMode(iface.Name)
-        if err != nil {
-            continue
-        }
-        fmt.Println("open interface", iface.Name)
-        return handle, nil
-    }
-
-    errs := ""
-    for i, iface := range ifs {
-        errs += fmt.Sprintf("\ndev %d: %s (%s)", i+1, iface.Name, iface.Description)
-    }
-
-    return nil, fmt.Errorf("failed to find monitor mode available interface.%s", errs)
-}
-
-func openAsMonitorMode(device string) (*pcap.Handle, error) {
-
-    inactive, err := pcap.NewInactiveHandle(device)
-    if err != nil {
-        return nil, fmt.Errorf("NewInactiveHandle(%s) failed: %s\n", device, err)
-    }
-    defer inactive.CleanUp()
-
-    // change mode to monitor
-    if err := inactive.SetRFMon(true); err != nil {
-        return nil, fmt.Errorf("SetRFMon failed: %s\n", err)
-    }
-
-    // create the actual handle by calling Activate:
-    handle, err := inactive.Activate() // after this, inactive is no longer valid
-    if handle == nil {
-        return nil, fmt.Errorf("Activate(%s) failed: %s\n", device, err)
-    }
-
-    return handle, nil
-}
 
 func main() {
     //// logging setup
@@ -83,51 +32,21 @@ func main() {
     device := flag.String("d", "", "device")
     flag.Parse()
 
-    // device name must be set
-    var handle *pcap.Handle
-    var err error
-    if *device == "" {
-        handle, err = openAvailableMonitorModeInterface()
-    } else {
-        handle, err = openAsMonitorMode(*device)
-    }
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer handle.Close()
-    fmt.Printf("pcap version: %s\n", pcap.Version())
-
     // decode const settings
     //if _, err := toml.DecodeFile("config.tml", &config); err != nil {
     //    fmt.Printf("load config.tml failed: %s\n", err)
     //    return
     //}
 
-    // start packet capturing
-    packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-    for packet := range packetSource.Packets() {
+	// start packet capturing
+	source, err := probe.NewProbeSource(*device)
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer source.Close()
 
-        // decode and find ProbeRequest
-        probeLayer := packet.Layer(layers.LayerTypeDot11MgmtProbeReq)
-        if probeLayer == nil {
-            //fmt.Println("other packet found", packet)
-            continue
-        }
-
-        dot11 := packet.Layer(layers.LayerTypeDot11).(*layers.Dot11)
-        radioTap := packet.Layer(layers.LayerTypeRadioTap).(*layers.RadioTap)
-
-        // send a record to DB
-        r := probe.ProbeRecord{
-            Time:       packet.Metadata().Timestamp,
-            Mac:        dot11.Address2.String(),
-            Rssi:       int(radioTap.DBMAntennaSignal),
-            SequenceId: int(dot11.SequenceNumber),
-            ApName:     config.Apname,
-        }
-        //postRecord(r)
-
-        log.Println(r)
+    for record := range source.Records() {
+        log.Println(record)
     }
 }
 
---
+++ probe/capture.go
@@ -0,0 +1,119 @@
+package probe
+
+import (
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+	"github.com/google/gopacket/layers"
+)
+
+type ProbeSource struct {
+	c    chan ProbeRecord
+	stop chan bool
+}
+
+func (s *ProbeSource)Records() chan ProbeRecord {
+	return s.c
+}
+
+func (s *ProbeSource)Close() {
+	s.stop <- true
+}
+
+func NewProbeSource(device string) (*ProbeSource, error) {
+
+	var handle *pcap.Handle
+	var err error
+	if device == "" {
+		handle, err = openAvailableMonitorModeInterface()
+	} else {
+		handle, err = openAsMonitorMode(device)
+	}
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("pcap version: %s\n", pcap.Version())
+
+	source := &ProbeSource{
+		c:    make(chan ProbeRecord),
+		stop: make(chan bool),
+	}
+
+	go func() {
+		defer handle.Close()
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for {
+			select {
+			case packet := <-packetSource.Packets():
+				// decode and find ProbeRequest
+				probeLayer := packet.Layer(layers.LayerTypeDot11MgmtProbeReq)
+				if probeLayer == nil {
+					continue
+				}
+
+				dot11 := packet.Layer(layers.LayerTypeDot11).(*layers.Dot11)
+				radioTap := packet.Layer(layers.LayerTypeRadioTap).(*layers.RadioTap)
+
+				source.c <- ProbeRecord{
+					Time:       packet.Metadata().Timestamp,
+					Mac:        dot11.Address2.String(),
+					Rssi:       int(radioTap.DBMAntennaSignal),
+					SequenceId: int(dot11.SequenceNumber),
+					ApName:     "undefined",
+				}
+			case <-source.stop:
+				break
+			}
+		}
+	}()
+
+	return source, nil
+}
+
+func openAvailableMonitorModeInterface() (*pcap.Handle, error) {
+
+	// find useable devices
+	ifs, err := pcap.FindAllDevs()
+	if len(ifs) == 0 {
+		return nil, fmt.Errorf("no devices found : %s\n", err)
+	}
+
+	// try any interfaces to monitor
+	for _, iface := range ifs {
+		handle, err := openAsMonitorMode(iface.Name)
+		if err != nil {
+			continue
+		}
+		fmt.Println("open interface", iface.Name)
+		return handle, nil
+	}
+
+	errs := ""
+	for i, iface := range ifs {
+		errs += fmt.Sprintf("\ndev %d: %s (%s)", i+1, iface.Name, iface.Description)
+	}
+
+	return nil, fmt.Errorf("failed to find monitor mode available interface.%s", errs)
+}
+
+func openAsMonitorMode(device string) (*pcap.Handle, error) {
+
+	inactive, err := pcap.NewInactiveHandle(device)
+	if err != nil {
+		return nil, fmt.Errorf("NewInactiveHandle(%s) failed: %s\n", device, err)
+	}
+	defer inactive.CleanUp()
+
+	// change mode to monitor
+	if err := inactive.SetRFMon(true); err != nil {
+		return nil, fmt.Errorf("SetRFMon failed: %s\n", err)
+	}
+
+	// create the actual handle by calling Activate:
+	handle, err := inactive.Activate() // after this, inactive is no longer valid
+	if handle == nil {
+		return nil, fmt.Errorf("Activate(%s) failed: %s\n", device, err)
+	}
+
+	return handle, nil
+}
