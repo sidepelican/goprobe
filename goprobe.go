183971d095a01548ffd6422cdc71d61cd2bc3a31
okamura_kenta <side.junktown@gmail.com>
2017-05-09 21:31:44 +0900

    not wait MQTT publish result and fix some error messages

--- goprobe.go
+++ goprobe.go
@@ -1,121 +1,133 @@
 package main
 
 import (
-	"log"
-	"os"
-	"io"
-	"flag"
-	"path"
-	"encoding/json"
-	"runtime"
-	//"net/http"
-	//"bytes"
+    "log"
+    "fmt"
+    "os"
+    "io"
+    "flag"
+    "path"
+    "encoding/json"
+    "runtime"
+    //"net/http"
+    //"bytes"
 
-	"github.com/BurntSushi/toml"
-	"github.com/sidepelican/goprobe/probe"
-	MQTT "github.com/eclipse/paho.mqtt.golang"
+    "github.com/BurntSushi/toml"
+    "github.com/sidepelican/goprobe/probe"
+    MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
 type Config struct {
-	ApiUrl  string
-	ApName  string
-	MqttUri string
+    ApiUrl  string
+    ApName  string
+    MqttUri string
 }
 
 func exists(filename string) bool {
-	_, err := os.Stat(filename)
-	return err == nil
+    _, err := os.Stat(filename)
+    return err == nil
 }
 
-func findDefaultConfigPath() string {
-	if runtime.GOOS == "linux" {
-		p := "/etc/goprobe/config.tml"
-		if exists(p) {
-			return p
-		}
-	}
+func findDefaultConfigPath() (string, error) {
 
-	runPath, err := os.Executable()
-	if err == nil {
-		return path.Dir(runPath) + "/config.tml"
-	}
+    errret := fmt.Errorf("config.tml not found at: ")
 
-	return ""
+    if runtime.GOOS == "linux" {
+        p := "/etc/goprobe/config.tml"
+        if exists(p) {
+            return p, nil
+        }
+        errret = fmt.Errorf("%v\n\t%v",errret, p)
+    }
+
+    runPath, err := os.Executable()
+    if err == nil {
+        p := path.Dir(runPath) + "/config.tml"
+        if exists(p) {
+            return p, nil
+        }
+        errret = fmt.Errorf("%v\n\t%v",errret, p)
+    }
+
+    return "", errret
 }
 
 func loadConfig(path string) (config Config) {
-	config.ApName = "undefined"
+    config.ApName = "undefined"
 
-	if path == "" {
-		path = findDefaultConfigPath()
-		if path == "" {
-			log.Println("cannot find config file path")
-		} else {
-			log.Println("load default config:", path)
-		}
-	}
+    if path == "" {
+        var err error
+        path, err = findDefaultConfigPath()
+        if err != nil {
+            log.Println(err)
+            return
+        }
+        log.Println("<config> load default path:", path)
+    }
 
-	// decode const settings
-	if _, err := toml.DecodeFile(path, &config); err != nil {
-		log.Println(err)
-	}
-	return
+    // decode const settings
+    if _, err := toml.DecodeFile(path, &config); err != nil {
+        log.Println(err)
+    }
+    return
 }
 
 func main() {
-	// logging setup
-	if runtime.GOOS == "linux" {
-		f, err := os.OpenFile("/var/log/goprobe.log", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
-		if err != nil {
-			println("error opening file: %v", err)
-		}
-		defer f.Close()
-		log.SetOutput(io.MultiWriter(f, os.Stdout)) // assign it to the standard logger
-	}
-	log.SetFlags(0)
+    // logging setup
+    log.SetFlags(0)
+    if runtime.GOOS == "linux" {
+        const logPath = "/var/log/goprobe.log"
+        f, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+        if err != nil {
+            println("error opening file: %v", err)
+        } else {
+            println("logging to ", logPath)
+            defer f.Close()
+            log.SetOutput(io.MultiWriter(f, os.Stdout)) // assign it to the standard logger
+        }
+    }
 
-	// parse flags
-	device := flag.String("d", "", "device")
-	configPath := flag.String("e", "", "config path")
-	flag.Parse()
+    // parse flags
+    device := flag.String("d", "", "device")
+    configPath := flag.String("e", "", "config path")
+    flag.Parse()
 
-	config := loadConfig(*configPath)
+    config := loadConfig(*configPath)
 
-	// start packet capturing
-	source, err := probe.NewProbeSource(*device, config.ApName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer source.Close()
+    // start packet capturing
+    source, err := probe.NewProbeSource(*device, config.ApName)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer source.Close()
 
-	// setup MQTT (optional)
-	var mqttClient MQTT.Client = nil
-	if config.MqttUri != "" {
-		opts := MQTT.NewClientOptions()
-		opts.AddBroker(config.MqttUri)
+    // setup MQTT (optional)
+    var mqttClient MQTT.Client = nil
+    if config.MqttUri != "" {
+        opts := MQTT.NewClientOptions()
+        opts.AddBroker(config.MqttUri)
 
-		mqttClient = MQTT.NewClient(opts)
-		if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
-			log.Println("MQTT:", token.Error())
-		} else {
-			defer mqttClient.Disconnect(250)
-			log.Println("MQTT Publisher Started")
-		}
-	}
+        mqttClient = MQTT.NewClient(opts)
+        if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
+            log.Println("MQTT:", token.Error())
+        } else {
+            defer mqttClient.Disconnect(250)
+            log.Println("MQTT Publisher Started")
+        }
+    }
 
-	for record := range source.Records() {
-		log.Println(record)
+    for record := range source.Records() {
+        log.Println(record)
 
-		bytes, err := json.Marshal(record)
-		if err != nil {
-			continue
-		}
+        bytes, err := json.Marshal(record)
+        if err != nil {
+            continue
+        }
 
-		if mqttClient != nil {
-			t := mqttClient.Publish("/goprobe", 2, false, bytes)
-			t.Wait()
-		}
-	}
+        if mqttClient != nil {
+            mqttClient.Publish("/goprobe", 2, false, bytes)
+        }
+    }
 }
 
 //func postRecord(record *ProbeRecord) {
